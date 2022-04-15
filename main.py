82feafdd9468937dfe06099cf0d14a830d34b329
MMarMAX <96263501+MMarMAX@users.noreply.github.com>
2022-04-15 18:59:06 +0300

    UI конвертера перенесено в класс (Пока не работает)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import tkinter
+
 import requests
 from tkinter import *
 import tkinter as tk
@@ -7,42 +9,34 @@ from tkinter import ttk
 
 
 class CurrencyConverter():
- def __init__(self,url):
+ def __init__(self, url):
   self.data = requests.get(url).json()
   self.currencies = self.data['rates']
- def convert(self,from_currency, to_currency,amount):
+ def convert(self, from_currency, to_currency,amount):
   initial_amount = amount
   if from_currency != 'USD':
     amount = amount/self.currencies[from_currency]
   amount = round(amount * self.currencies[to_currency], 4)
   return amount
 
-url='https://api.exchangerate-api.com/v4/latest/USD'
-converter= CurrencyConverter(url)
-print(converter.convert('RUB','USD',10000))
+url = 'https://api.exchangerate-api.com/v4/latest/USD'
+converter = CurrencyConverter(url)
+print(converter.convert('RUB', 'USD', 10000))
 
 
 
+class ConvereterUI:
+ def __init__(self, converter):
+  Tk.__init__(self)
+  self.title = "Converter"
+  self.converter = converter
+  self.intro = Label(self, text="Конвертер валют", fg='blue', borderwidth=3, font=('Times New Roman', 16))
+  self.date = Label(self, text=f"1 индийская рупия = {self.converter.convert('IND','USD',1)} USD \n Date:{self.converter.data['date']}",
+                    relief=tkinter.GROOVE, borderwidth=5)
+  self.intro.place(x=20, y=10)
+  self.date.place(x=170, y=50)
 
 
-
-
-
-
-
-
-
-
-root=Tk()
-root.title = 'Converter'
-root.geometry("500x200")
-  #Labels
-
-intro = Label(root,text='Конвертер валют',fg='blue',relief=tk.RAISED, borderwidth=3)
-intro.config(font = ('Times New Roman',15,'bold'))
-date = Label(root,text='1 USD = 82.5 RUB \n Date:14.04.2022 ', relief=tk.GROOVE,borderwidth=5)
-intro.place(x=50,y=5)
-date.place(x=170, y=50)
 
 
 
