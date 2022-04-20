2151f1cdb9dc5bc81c5f38fac00d2ff1b1c817f4
MMarMAX <96263501+MMarMAX@users.noreply.github.com>
2022-04-20 18:43:31 +0300

    Сделано поле ввода валюты и выпадающие списки с валютой

--- main.py
+++ main.py
@@ -19,22 +19,48 @@ class CurrencyConverter():
   amount = round(amount * self.currencies[to_currency], 4)
   return amount
 
-url = 'https://api.exchangerate-api.com/v4/latest/USD'
-converter = CurrencyConverter(url)
-print(converter.convert('RUB', 'USD', 10000))
+ url = 'https://api.exchangerate-api.com/v4/latest/USD'
 
 
-
-class ConvereterUI:
+class App(tk.Tk):
  def __init__(self, converter):
-  Tk.__init__(self)
+  tk.Tk.__init__(self)
   self.title = "Converter"
   self.converter = converter
   self.intro = Label(self, text="Конвертер валют", fg='blue', borderwidth=3, font=('Times New Roman', 16))
-  self.date = Label(self, text=f"1 индийская рупия = {self.converter.convert('IND','USD',1)} USD \n Date:{self.converter.data['date']}",
+  self.date = Label(self, text=f"1 индийская рупия = {self.converter.convert('IND','USD',1)} USD \n Дата:{self.converter.data['date']}",
                     relief=tkinter.GROOVE, borderwidth=5)
-  self.intro.place(x=20, y=10)
-  self.date.place(x=170, y=50)
+  self.intro.place(x=125, y=10)
+  self.date.place(x=125, y=50)
+
+  #Создание полей для ввода валют
+  valid = (self.register(self.restrictNumberOnly), '%d', '%P')
+  self.amount_field = Entry(self, bd=3, justify=tk.CENTER, validate='key', validatecommand=valid)
+  self.converted_amount_field = Label(self, text='', fg='black', bg='white',justify=tk.CENTER, width=16, borderwidth=3)
+
+  #Значения по умолчанию
+  self.from_currency_value = StringVar(self)
+  self.from_currency_value.set('RUB')
+  self.to_currency_value = StringVar(self)
+  self.to_currency_value.set('USD')
+
+  font=('Times New Roman', 12, 'bold')
+  self.option_add('*TCombobox*Listbox.font', font)
+  self.from_currency_dropdown = ttk.Combobox(self, textvariable=self.from_currency_value,
+                                             values=list(self.converter.currencies.keys()), font=font,
+                                             state='readonly', width=12, justify=tk.CENTER)
+  self.to_currency_dropdown = ttk.Combobox(self, textvariable=self.to_currency_value,
+                                           values=list(self.converter.currencies.keys()), font=font,
+                                           state='readonly', width=12, justify=tk.CENTER)
+
+
+
+
+
+
+
+
+
 
 
 
